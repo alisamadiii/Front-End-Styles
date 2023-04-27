c1a882f070025920d93830eb9250645cfe5b5a8e
Ali Reza <webdeve1083@gmail.com>
2023-04-28 05:48:20 +0800

    Design-5 is Done✅

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,75 +1,150 @@
 import { Inter } from "next/font/google";
-import "vercel-toast/css";
-import { createToast } from "vercel-toast";
-import Button from "./components/Button";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 const inter = Inter({ subsets: ["latin"] });
 
+import { LINKS } from "./contents/Data";
+import Link from "next/link";
+
+import { IoIosArrowDown } from "react-icons/io";
+import { AiOutlineLink } from "react-icons/ai";
+
 export default function Home() {
-  const success = () => {
-    createToast(
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      {
-        timeout: 3000, // in 3 seconds
-        type: "success",
-      }
-    );
-  };
-
-  const warning = () => {
-    createToast(
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      {
-        timeout: 3000, // in 3 seconds
-        type: "warning",
-      }
-    );
-  };
-
-  const error = () => {
-    createToast(
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      {
-        timeout: 3000, // in 3 seconds
-        type: "error",
-      }
-    );
-  };
-
-  const dark = () => {
-    createToast(
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      {
-        timeout: 3000, // in 3 seconds
-        type: "dark",
-      }
-    );
-  };
-
-  const actionCancel = () => {
-    createToast(
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      {
-        action: {
-          text: "Undo",
-          callback(toast) {
-            toast.destroy();
-          },
-        },
-        cancel: "Cancel",
-      }
-    );
-  };
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
+  const [isOpen3, setIsOpen3] = useState(false);
 
   return (
-    <main className={`${inter.className}`}>
-      <h1 className="py-24 text-5xl font-black text-center">Vercel Toast</h1>
-      <div className="flex items-center justify-center h-[20vh] gap-4 ">
-        <Button onClick={success}>Success</Button>
-        <Button onClick={warning}>Warning</Button>
-        <Button onClick={error}>Error</Button>
-        <Button onClick={dark}>Dark</Button>
-        <Button onClick={actionCancel}>Action + Cancel </Button>
+    <main
+      className={`${inter.className} flex justify-center items-center h-screen gap-8`}
+    >
+      <div>
+        <motion.button
+          layout="position"
+          className="flex items-center justify-between p-2 bg-white rounded-md shadow-lg w-52"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <span>Menu</span>
+          <IoIosArrowDown
+            className={`duration-200 ${isOpen && "rotate-180"}`}
+          />
+        </motion.button>
+        <AnimatePresence>
+          {isOpen && (
+            <div className="flex flex-col p-2 mt-2 overflow-hidden bg-white rounded-md shadow-xl w-52">
+              {LINKS.map((link) => {
+                const Icon = link.icon;
+
+                return (
+                  <motion.div
+                    key={link.link}
+                    initial={{ opacity: 0, x: 20 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.3, delay: link.link * 0.05 }}
+                    exit={{ opacity: 0, x: 20 }}
+                  >
+                    <Link
+                      href={link.href}
+                      className="flex items-center gap-2 p-2 hover:bg-[#f2f2f2] duration-150 rounded-md"
+                    >
+                      <Icon />
+                      <span className="text-sm">{link.name}</span>
+                      <AiOutlineLink className="ml-auto " />
+                    </Link>
+                  </motion.div>
+                );
+              })}
+            </div>
+          )}
+        </AnimatePresence>
+      </div>
+
+      <div>
+        <motion.button
+          layout="position"
+          className="flex items-center justify-between p-2 bg-white rounded-md shadow-lg w-52"
+          onClick={() => setIsOpen2(!isOpen2)}
+        >
+          <span>Menu</span>
+          <IoIosArrowDown
+            className={`duration-200 ${isOpen2 && "rotate-180"}`}
+          />
+        </motion.button>
+        <AnimatePresence>
+          {isOpen2 && (
+            <div className="absolute flex flex-col p-2 mt-2 bg-white rounded-md shadow-xl w-52">
+              {LINKS.map((link) => {
+                const Icon = link.icon;
+
+                return (
+                  <motion.div
+                    key={link.link}
+                    initial={{ opacity: 0, x: 20 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.3, delay: link.link * 0.05 }}
+                    exit={{ opacity: 0, x: 20 }}
+                  >
+                    <Link
+                      href={link.href}
+                      className="flex items-center gap-2 p-2 hover:bg-[#f2f2f2] duration-150 rounded-md"
+                    >
+                      <Icon />
+                      <span className="text-sm">{link.name}</span>
+                      <AiOutlineLink className="ml-auto " />
+                    </Link>
+                  </motion.div>
+                );
+              })}
+            </div>
+          )}
+        </AnimatePresence>
+      </div>
+
+      <div>
+        <motion.button
+          layout="position"
+          className="flex items-center justify-between p-2 bg-white rounded-md shadow-lg w-52"
+          onClick={() => setIsOpen3(!isOpen3)}
+        >
+          <span>Menu</span>
+          <IoIosArrowDown
+            className={`duration-200 ${isOpen3 && "rotate-180"}`}
+          />
+        </motion.button>
+        <AnimatePresence>
+          {isOpen3 && (
+            <motion.div
+              initial={{ height: 0, padding: 0 }}
+              animate={{ height: "auto", padding: 8 }}
+              exit={{ height: 0, padding: 0 }}
+              className="flex flex-col px-2 mt-2 overflow-hidden bg-white rounded-md shadow-xl w-52"
+            >
+              {LINKS.map((link) => {
+                const Icon = link.icon;
+
+                return (
+                  <motion.div
+                    key={link.link}
+                    initial={{ opacity: 0, x: 20 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 0.3, delay: link.link * 0.05 }}
+                    exit={{ opacity: 0, x: 20 }}
+                  >
+                    <Link
+                      href={link.href}
+                      className="flex items-center gap-2 p-2 hover:bg-[#f2f2f2] duration-150 rounded-md"
+                    >
+                      <Icon />
+                      <span className="text-sm">{link.name}</span>
+                      <AiOutlineLink className="ml-auto " />
+                    </Link>
+                  </motion.div>
+                );
+              })}
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </main>
   );
