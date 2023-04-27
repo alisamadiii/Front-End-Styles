110aeb43db53b8ef67ac095e11e49c5bd7921ed4
Ali Reza <webdeve1083@gmail.com>
2023-04-27 17:23:50 +0800

    Design-4 is Done ✅

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,75 @@
 import { Inter } from "next/font/google";
-
-import Card from "./components/Card"; // Components
-import { DATA } from "./contents/Data"; // Array
+import "vercel-toast/css";
+import { createToast } from "vercel-toast";
+import Button from "./components/Button";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const success = () => {
+    createToast(
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      {
+        timeout: 3000, // in 3 seconds
+        type: "success",
+      }
+    );
+  };
+
+  const warning = () => {
+    createToast(
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      {
+        timeout: 3000, // in 3 seconds
+        type: "warning",
+      }
+    );
+  };
+
+  const error = () => {
+    createToast(
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      {
+        timeout: 3000, // in 3 seconds
+        type: "error",
+      }
+    );
+  };
+
+  const dark = () => {
+    createToast(
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      {
+        timeout: 3000, // in 3 seconds
+        type: "dark",
+      }
+    );
+  };
+
+  const actionCancel = () => {
+    createToast(
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      {
+        action: {
+          text: "Undo",
+          callback(toast) {
+            toast.destroy();
+          },
+        },
+        cancel: "Cancel",
+      }
+    );
+  };
+
   return (
-    <main className={`${inter.className} flex justify-center py-12`}>
-      <div className="flex flex-wrap items-center justify-center gap-4">
-        {DATA.map((data) => (
-          <Card key={data.id} data={data} />
-        ))}
+    <main className={`${inter.className}`}>
+      <h1 className="py-24 text-5xl font-black text-center">Vercel Toast</h1>
+      <div className="flex items-center justify-center h-[20vh] gap-4 ">
+        <Button onClick={success}>Success</Button>
+        <Button onClick={warning}>Warning</Button>
+        <Button onClick={error}>Error</Button>
+        <Button onClick={dark}>Dark</Button>
+        <Button onClick={actionCancel}>Action + Cancel </Button>
       </div>
     </main>
   );
